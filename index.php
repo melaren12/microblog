6306fb6ab3b0491d07ec6f5a3e45ace986fc6860
melaren12 <melaren6@gmail.com>
2025-03-19 18:33:39 +0400

    error correction

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
     <link rel="stylesheet" href="CSS/styles.css">
 </head>
 <body>
-<div class="main-container">
-    <div class="container">
-        <h1>Welcome!!</h1>
-        <div class="links">
-            <a href="register.php"><button class="btn">Register</button></a>
-            <a href="login.php"><button class="btn">Login</button></a>
+    <div class="main-container">
+        <div class="container">
+            <h1>Welcome!!</h1>
+            <div class="links">
+                <a href="register.php"><button class="btn">Register</button></a>
+                <a href="login.php"><button class="btn">Login</button></a>
+            </div>
         </div>
     </div>
-</div>
 </body>
 </html>
